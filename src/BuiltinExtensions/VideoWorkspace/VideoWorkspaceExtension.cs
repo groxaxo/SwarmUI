7881bc34a76933bccf38a64c0cbec3107bb5a853
Vlad Jagodnik <blackstoreonline@gmail.com>
2026-08-18 11:41:20 +1200

    load video workspace modules in deterministic order

--- src/BuiltinExtensions/VideoWorkspace/VideoWorkspaceExtension.cs
+++ src/BuiltinExtensions/VideoWorkspace/VideoWorkspaceExtension.cs
@@ -8,7 +8,20 @@ public class VideoWorkspaceExtension : Extension
     /// <inheritdoc/>
     public override void OnPreInit()
     {
-        ScriptFiles.Add("Assets/video_workspace.js");
+        ScriptFiles.Add("Assets/video_workspace_00_base.js");
+        ScriptFiles.Add("Assets/video_workspace_01_methods.js");
+        ScriptFiles.Add("Assets/video_workspace_02_methods.js");
+        ScriptFiles.Add("Assets/video_workspace_03_methods.js");
+        ScriptFiles.Add("Assets/video_workspace_04_methods.js");
+        ScriptFiles.Add("Assets/video_workspace_05_methods.js");
+        ScriptFiles.Add("Assets/video_workspace_06_methods.js");
+        ScriptFiles.Add("Assets/video_workspace_07_methods.js");
+        ScriptFiles.Add("Assets/video_workspace_08_methods.js");
+        ScriptFiles.Add("Assets/video_workspace_09_methods.js");
+        ScriptFiles.Add("Assets/video_workspace_10_methods.js");
+        ScriptFiles.Add("Assets/video_workspace_11_methods.js");
+        ScriptFiles.Add("Assets/video_workspace_12_methods.js");
+        ScriptFiles.Add("Assets/video_workspace_13_init.js");
         StyleSheetFiles.Add("Assets/video_workspace.css");
     }
 }
